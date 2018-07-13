ed8e85aa5033dd3b79ded178759fea9d47f8f586
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-07-13 18:45:38 -0500

    Flex layoud added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatModule } from './mat.module';
+import { FlexModule } from '@angular/flex-layout';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FlexModule
   ],
-  exports: [RouterModule, MatModule],
+  exports: [RouterModule, MatModule, FlexModule],
   declarations: []
 })
 export class SharedModule { }
