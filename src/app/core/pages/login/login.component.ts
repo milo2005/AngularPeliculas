5435957cbf7383e5809d768c2cdc3572d2e79b3a
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-07-16 19:03:29 -0500

    Login implemented

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = '';
+  password = '';
+
+  error = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      this.router.navigate(['estrenos']);
+    } else {
+      this.error = true;
+      setTimeout(() => this.error = false, 1000);
+    }
   }
 
 }
